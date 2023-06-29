e5c5704506d27f7841a389a82f7ff30a3d7366a3
Luke Wainhouse <lukewainhouse@hotmail.com>
2023-06-29 16:24:13 +0100

    feat: user can import data

--- controllers/uploadUserAddressesController.js
+++ controllers/uploadUserAddressesController.js
@@ -1,10 +1,31 @@
-import UserAddress from '../models/UserAddressData.js';
+import mongoose from 'mongoose';
+import userAddressSchema from '../schemas/UploadUserAdresses.js'
+
+
+const UserAddressData = '../models/UserAddressData.js'
 
 export async function uploadUserAddressData(req, res) {
-  const { addressData, username } = req.body;
+  const addressData = req.body;
+  const username = req.user
+  console.log({ username });
+  const user = username.username
+  const collectionName = `${user}_addresses`;
+  console.log({ user });
+
+
+
+
   try {
+    const collectionExists = await mongoose.connection.db.listCollections({ name: collectionName }).hasNext();
+    if (!collectionExists) {
+      await mongoose.connection.db.createCollection(collectionName);
+    }
+
+    const UserAddressDynamic = mongoose.model('UserAddressData', userAddressSchema, collectionName);
+
     // Insert the addresses into the user address database
-    const insertedUserAddressData = await UserAddress.insertMany(username, addressData);
+    const insertedUserAddressData = await UserAddressDynamic.insertMany(addressData);
+
 
     res.status(201).json({ message: 'User address data uploaded successfully.', userAddressData: insertedUserAddressData });
   } catch (err) {
